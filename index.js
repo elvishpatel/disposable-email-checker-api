4376b711fa77984bd9464e765dd8ace02c1f7251
Elvish Kansagara <129504985+elvishpatel@users.noreply.github.com>
2026-05-23 11:26:24 +0530

    Add 'null' to allowedOrigins in index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
   : [
       'http://localhost:3000',
-      'http://127.0.0.1:5500'
+      'http://127.0.0.1:5500',
+      'null'
     ];
 
 app.use(cors({
